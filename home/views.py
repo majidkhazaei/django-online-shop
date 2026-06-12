34b7e3d19324978ed5d20406640a11ad8a427812
Majid <majidtriste1@gmail.com>
2026-06-12 12:00:54 +0330

    added category filter system

--- home/views.py
+++ home/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from products.models import Product
+from products.models import Product, Category
 from . import tasks
 from django.contrib import messages
 from utils import IsAdminMixin
 
 
 class HomeView(View):
-    def get(self, request):
+    def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, 'home/index.html', {'products': products})
+        categories = Category.objects.all()
+        if category_slug:
+            category = Category.objects.get(slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, 'home/index.html', {'products': products, 'categories': categories})
 
 
 class ProductDetailView(View):
